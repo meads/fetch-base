2f0387fe485cb7eab1d9a644e7813ef2e3dfb684
Mike Eads <me@mikeads.com>
2018-02-01 19:54:26 -0600

    Adds FetchBase with tsconfig;

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import * as fetch from "whatwg-fetch";
+
+export interface FetchBase<T> {
+    get() : Promise<T[]>;
+    put(item: T) : Promise<Object>;
+    post(item: T) : Promise<Object>;
+    delete(item: T) : Promise<boolean>;
+}
+
+export class FetchConfig {
+    constructor(
+        public ip: string,
+        public api: string,
+        public version: string,
+        public protocol: "http" | "https",
+    ){}
+    getUrl(): string {
+        return `${this.protocol}://${this.api}/${this.version}/`;
+    }
+}
+
+export class FetchBase<T> implements FetchBase<T> {
+    protected endpoint: string = "";
+    protected cors: "cors" | "no-cors";
+    private requestUri: string = "";
+
+    constructor(private config: FetchConfig){}
+
+    get() : Promise<T[]> {
+        this.requestUri = `${this.config.getUrl()}${this.endpoint}`;
+        return fetch(this.requestUri, { cors: this.cors }).then((response: Response) => {
+            if(!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then((json: JSON) => { return json })
+        .catch((reason: Error) => {
+            return Promise.reject(reason);
+        })
+    }
+}
